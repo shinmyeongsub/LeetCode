5b90da7e36f5791c1db0ab43aac1383a17e1b693
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2023-08-03 23:37:12 +0900

    Delete Trash Files

--- Solution/Easy/MergeStringsAlternately.java
+++
@@ -1,9 +0,0 @@
-package Solution.Easy;
-
-// 1768. Merge Strings Alternately
-public class MergeStringsAlternately {
-    public String mergeAlternately(String word1, String word2) {
-        System.out.println("TEST");
-        return word1 + word2;
-    }
-}
--- Solution/ServiceLauncher.java
+++
@@ -1,8 +0,0 @@
-package Solution;
-
-import Solution.Easy.MergeStringsAlternately;
-
-public class ServiceLauncher {
-    public static void main(String[] args) {
-    }
-}
